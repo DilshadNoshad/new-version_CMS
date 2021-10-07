5b6bd840c5aa2047dcd97a3a94d46516b1a21fd5
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-07 13:06:37 +0500

    fetch objects

--- blog.php
+++ blog.php
@@ -14,12 +14,14 @@
               while($row = mysqli_fetch_assoc($select_all_posts)){
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
+                $post_author_image = $row['post_author_image'];
                 $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
                 $post_content = $row['post_content'];
             ?>
               <!-- post -->
               <div class="post col-xl-6">
-                <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-1.jpeg" alt="..." class="img-fluid"></a></div>
+                <div class="post-thumbnail"><a href="post.html"><img src="img/<?php echo $post_image ?>" alt="..." class="img-fluid"></a></div>
                 <div class="post-details">
                   <div class="post-meta d-flex justify-content-between">
                     <div class="date meta-last"><?php echo $post_date ?></div>
@@ -28,7 +30,7 @@
                     <h3 class="h4"><?php echo $post_title ?></h3></a>
                   <p class="text-muted"><?php echo $post_content ?></p>
                   <footer class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
-                      <div class="avatar"><img src="img/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                      <div class="avatar"><img src="img/<?php echo $post_author_image ?>" alt="..." class="img-fluid"></div>
                       <div class="title"><span><?php echo $post_author ?></span></div></a>
                     <div class="date"><i class="icon-clock"></i> 2 months ago</div>
                     <div class="comments meta-last"><i class="icon-comment"></i>12</div>
