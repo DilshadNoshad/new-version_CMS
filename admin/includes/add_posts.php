b04f7e07050830fba8aa532238c169f82c7756b0
DilshadSheikh <dilshadnoshad2003@gmail.com>
2021-10-14 03:03:19 +0500

    check images post or not

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -1,4 +1,22 @@
+<?php
+if(isset($_POST['create_post'])){
+$post_title =  $_POST['title'];
+$post_author =  $_POST['author'];
+$post_category_id =  $_POST['post_category_id'];
+$post_status =  $_POST['post_status'];
 
+$post_image = $_FILES['post_image']['name'];
+$post_image_temp = $_FILES['post_image']['tmp_name'];
+
+$post_tags =  $_POST['post_tags'];
+$post_content =  $_POST['post_content'];
+
+$post_date =  date('d-m-y');
+$post_comment_count =  4;
+
+    move_uploaded_file($post_image_temp, "../img/$post_image");
+}
+?>
     <form method="post" enctype="multipart/form-data">
 
     <div class="col-md-9 col-sm-12  form-group">
